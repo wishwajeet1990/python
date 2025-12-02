2fda7c7808e7679814e92d2769865bdcde3c4739
Wishwajeet <Wishwajeetrana1990@gmail.com>
2025-12-02 15:41:25 +0530

    Chech wether we can delete or not a perticular line from file or not . But still pending that operation

--- Basic of Pyhotn/tempCodeRunnerFile.py
+++ Basic of Pyhotn/tempCodeRunnerFile.py
@@ -1,1 +1,26 @@
-greet
+file_path = r"E:\Training\Python\Codding\Basic of Pyhotn\sample.txt"
+line_number = 3   # 1-based index
+
+f = open(f"{file_path}", "w")
+for i in range(5):
+    f.write(f"This is my simple text file that contain my simple text {i+1}\n")
+    f.flush()
+f.close()
+
+f = open(f"{file_path}", "r")
+for i in range(5):
+    line = f.readline()
+    print(line)
+f.close()
+
+f = open(f"{file_path}", "a+")
+lines =  f.readlines()
+for i, line in enumerate(lines, start=1):
+    if i != line_number:
+        f.write(line)
+f.close()
+
+print()
+print()
+f = open(f"{file_path}", "r")
+print(f.read())
--- IPC in python/1.Process or equal to Fork/1. create process using fork.py
+++ IPC in python/1.Process or equal to Fork/1. create process using fork.py
@@ -3,13 +3,14 @@
 
 import multiprocessing as mp
 import os 
+import time as t
 print(f"Process Started {__name__}")        #here module name will be __main__
 
 def child_process():
     print(f"Parent Process ID =  {os.getppid()}\nChild Process ID  =  {os.getpid()} ")
     # print(__name__)
     
-if __name__ == "__main__":          # We use this line because when the ne process is being created 
+if __name__ == "__main__":          # We use this line because when the new process is being created 
                                     #it start executing by re importing the main module again and again 
                                     #So to avoid the re importing we use this line because every module has is own name 
     print(f"Process ID  = {os.getpid()}")
@@ -17,5 +18,7 @@ if __name__ == "__main__":          # We use this line because when the ne proce
         new_process = mp.Process(target=child_process)      #here module name will be __mp_main__
         new_process.start()         #This tells Python to start the new process
         new_process.join()          #This tells the parent process to wait until the child process finishes
+        t.sleep(1)
+        print()
     print("Child process finished")
     
